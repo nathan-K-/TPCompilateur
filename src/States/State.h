dc3b10cec0f63e5ceae427c61ea3173e0a11f91a
nathan-k- <nathan.kienlen@gmail.com>
2017-02-15 16:10:05 +0100

    adding hello world

---
+++ src/States/State.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+                                    State
+                             -------------------
+    début                : 17.02
+    copyright            : (C) 2017 par HAMOUMA Naima et KIENLEN Nathan
+*************************************************************************/
+
+#ifndef TPCOMPILATEUR_STATE_H
+#define TPCOMPILATEUR_STATE_H
+
+
+class State {
+
+};
+
+
+#endif //TPCOMPILATEUR_STATE_H
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+                                    main
+                             -------------------
+    début                : 17.02
+    copyright            : (C) 2017 par HAMOUMA Naima et KIENLEN Nathan
+*************************************************************************/
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello world !" << std::endl;
+    return 0;
+}
