3f8dc75be1eea4452899a2be1e4060202db0deb1
nathan-k- <nathan.kienlen@gmail.com>
2017-02-17 10:24:50 +0100

    ajout de nombre

---
+++ src/Nombre.h
@@ -0,0 +1,22 @@
+/*************************************************************************
+                                    Nombre
+                             -------------------
+    début                : 17.02
+    copyright            : (C) 2017 par HAMOUMA Naima et KIENLEN Nathan
+*************************************************************************/
+
+#ifndef TPCOMPILATEUR_NOMBRE_H
+#define TPCOMPILATEUR_NOMBRE_H
+
+
+#include "Symbole.h"
+
+class Nombre : public Symbole {
+public:
+
+    Nombre(int id) : Symbole(id) {}
+
+};
+
+
+#endif //TPCOMPILATEUR_NOMBRE_H
